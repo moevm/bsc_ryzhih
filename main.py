78a40720d45a9d21c02afa90b8752e146e4c0b37
Ryzhikh-Roman <54955488+Ryzhikh-Roman@users.noreply.github.com>
2022-12-16 00:31:44 +0300

    Add giving roles with reactions

--- main.py
+++ main.py
@@ -1,33 +1,69 @@
 import discord
+#import datetime
 
-TOKEN = 'MTA1MDU2Mzk5NzM2OTM3MjcyMw.GbWfLI.zWb-deGY8N9SBAp6YNtUoiJjCOvz-BfFWwpn1g'
-
+TOKEN = ''
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
-#client = discord.Client()
+
+#current_year = datetime.datetime.now().year
+
+class DiscordBot(discord.Client):
+    async def on_ready(self):
+        print('We have successfully loggged in as {0.user}'.format(self))
+
+    async def on_message(self, message):
+        if message.author == client.user:
+            return
+
+        if message.content.lower() == 'hello':
+            await message.channel.send(f'Hello, {message.author.display_name}!')
+
+            return
+
+        if message.content.lower() == 'bye':
+            await message.channel.send(f'See you later, {message.author.display_name}!')
+
+            return
+
+    async def on_raw_reaction_add(self, payload):
+        channel = client.get_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id)
+        guild = client.get_guild(payload.guild_id)
+        reaction = discord.utils.get(message.reactions, emoji=payload.emoji.name)
+        print(reaction)
+
+        if payload.member.id == client.user.id:
+            return
+
+        if payload.message_id == 1053040934671101983:
+            print(payload.member.roles)
+            match reaction.emoji:
+                case '🟥':
+                    role = discord.utils.get(guild.roles, name='role1')
+                case '🟨':
+                    role = discord.utils.get(guild.roles, name='role2')
+                case '🟦':
+                    role = discord.utils.get(guild.roles, name='role3')
+                case _:
+                    role = None
+
+            for roles in payload.member.roles:
+                if roles == discord.utils.get(guild.roles, name='@everyone'):
+                    continue
+
+                await payload.member.remove_roles(roles)
+
+            if role:
+                await payload.member.add_roles(role)
+
+            await reaction.remove(payload.member)
+
+    async def on_member_join(self, member):
+        guild = self.get_guild()
+        await member.add_roles(discord.utils.get(guild.roles, name='2022'))
 
 
-@client.event
-async def on_ready():
-    print('We have successfully loggged in as {0.user}'.format(client))
-
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.lower() == 'hello':
-        await message.channel.send(f'Hello, {message.author.display_name}!')
-
-        return
-
-    if message.content.lower() == 'bye':
-        await message.channel.send(f'See you later, {message.author.display_name}!')
-
-        return
-
+client = DiscordBot(intents=intents)
 
 client.run(TOKEN)
